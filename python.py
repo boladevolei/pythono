f895e477d81b71552cd8bf0990bc9befd3d74897
boladevolei <bobinhobobo05@gmail.com>
2025-03-24 08:47:26 -0300

    Atualização do código em python

---
+++ python.py
@@ -0,0 +1,8 @@
+idade=int(input("Qual a sua idade?"))
+if idade<16:
+    print("não é preciso votar")
+elif(idade==16 or >65):
+    print("voto não obrigatório")
+else:
+    print("voto obrigatório")
+
