b85ddfede7287306876298aab7228ab142f670c6
boladevolei <bobinhobobo05@gmail.com>
2025-04-01 12:19:26 -0300

    nurse

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -54,3 +54,46 @@ def saque(saque,saldo):
             numero_saques += 1
             
         return saldo        
+
+def saque(saque,saldo):
+
+
+   global  numero_saques,limites,limites_saques
+    #De acordo com a documentação Python , váriaveis globais não ficam presas ao limite do escopo da função.
+   if numero_saques >= limites_saques:
+    print('Você atingiu o limite de saques de sua conta.Tente novamente no proximo dia útil')
+   else:
+        if saque <= 0:
+            print('Saque Inválido.Verifique o valor e tente novamente.')
+        elif saque > limite :
+            print('Valor limite excedido.Verifique o valor e tente novamente.')
+        elif saque > saldo :
+            print('Saldo insuficiênte.Verifique o valor e tente novamente.')
+        else:
+            saldo -= saque
+            numero_saques += 1
+            
+        return saldo   
+
+def extrato (saldo):  
+    hora = datetime.now() #Obtem a hora atual 
+    horaatual = hora.strftime('%d/%m/%Y/ %H:%M')
+    print('=============Extrato=============')
+    print(f'{horaatual} \nSaldo disponível:{saldo}' )
+    print('\n=============Extrato=============')
+
+def sari():
+    print('Encerrando o Sistema')
+def novo_usuario
+    cpf = int(input('Digite seu cpf(Somente números);'))
+    usuario = filtrar_usuarios(cpf,usuarios)
+
+    if usuarios:
+    print('Usuário já cadastrado na base de dados')
+    return
+    nome = str(input('Escreva seu nome completo:'))
+    data_nascimento = input('Informe a data de nascimento (dd -- mm -- aa):')
+    endereço = input('Informe seu endereço (Rua, Número, Cep, bairro e cidade)')
+
+usuarios.append(f'nome:{nome}, data de nascimento:{data_nascimento} e endereço:{endereço}')
+print('Cadastro realizado com sucesso')
